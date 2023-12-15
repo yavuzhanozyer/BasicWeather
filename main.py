3d5951f638e2acbcc28adc8f22bc419ee77e9c47
Yavuzhan Özyer <cemsitfb@gmail.com>
2023-12-15 23:44:15 +0300

    first version

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+api_key = "00a644487487898add2523796d1e3b5e"
+while True:
+    try:
+        city = input("Şehir: ")
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=tr"
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        print(f"Şehir: {data['name']}")
+        print(f"Sıcaklık: {data['main']['temp']} °C")
+        print(f"Nem: {data['main']['humidity']} %")
+        print(f"Basınç: {data['main']['pressure']} hPa")
+        print(f"Rüzgar: {data['wind']['speed']} m/s")
+        print(f"Durum: {data['weather'][0]['description']}")
+        break
+    except KeyError:
+        print("Şehir adını doğru giriniz...")
